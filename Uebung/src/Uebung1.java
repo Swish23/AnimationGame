39544a0545c9734bf9b8996c71009c0bd11d2cf4
Sauer.Fabian <Sauer.Fabian@SK-448A5B5B575E>
2015-10-02 09:08:18 +0200

    Übung zum Bool´schen Operator

--- Uebung/src/Uebung1.java
+++ Uebung/src/Uebung1.java
@@ -2,7 +2,11 @@
 public class Uebung1 {
 
 	public static void main(String[] args) {
-		System.out.println("Hallo Welt");
+		System.out.println(Wahroderfalsch());
+	}
+	
+	static boolean Wahroderfalsch(){
+		return (4==4 || 5==5);
 	}
 
 }
