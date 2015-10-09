b50c8ddf49b043d94dfc22ab3a789520277c0860
Sauer.Fabian <Sauer.Fabian@SK-448A5B5B575E>
2015-10-09 08:56:32 +0200

    Fibonacci

--- Uebung/src/Uebung1.java
+++ Uebung/src/Uebung1.java
@@ -1,12 +1,19 @@
 
 public class Uebung1 {
-
-	public static void main(String[] args) {
-		System.out.println(Wahroderfalsch());
-	}
 	
-	static boolean Wahroderfalsch(){
-		return (4==4 || 5==5);
-	}
-
-}
+			static int fac(int i) {
+				return i==0?1:i*fac(i-1);
+				}
+				public static void main(String[] args) {
+				System.out.println("fac(5) = "+fac(5));
+			
+				System.out.println(fibonacci(x)+", ");}
+				
+			static long fibonacci(long x){
+				if(x<=0){return 0;}
+					else {
+					if(x==1 || x==2){return 1;}
+						else{return(fibonacci(x-2)+fibonacci(x-1));}
+				}
+			}
+		}
