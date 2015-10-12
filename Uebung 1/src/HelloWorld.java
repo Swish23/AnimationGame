61a0b2a16a458494971d0af250bfa39ea3a1b211
Swish23 <fabian.sauer98@hotmail.de>
2015-10-12 14:41:13 +0200

    Collatz

---
+++ Uebung 1/src/HelloWorld.java
@@ -0,0 +1,20 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		double n=123456;
+
+		while (n>1) {
+
+		if (n%2==0) {
+		n=n/2;
+		}
+		else {
+		n=3*n+1;	
+		}
+		System.out.println(n);
+		}
+
+	}
+
+}
