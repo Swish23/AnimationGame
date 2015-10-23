14755ddbb31176dd546d11322a176def894e79b4
Sauer.Fabian <Sauer.Fabian@SK-448A5B5B575E>
2015-10-23 07:52:22 +0200

    Commit

---
+++ AnimationGame/src/JFrameTest.java
@@ -0,0 +1,13 @@
+
+import javax.swing.JFrame;
+public class JFrameTest {
+	
+	public static void main(String[] args){
+		JFrame f=new JFrame("Hier könnte Ihre Werbung stehen");
+		f.setVisible(true);
+	
+	
+	}
+	
+	
+}
