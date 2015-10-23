f6c31709a0a99f276a1ac80e3982ae516340b0a4
Sauer.Fabian <Sauer.Fabian@SK-448A5B5B575E>
2015-10-23 08:14:26 +0200

    SimpleSizedPanel

---
+++ AnimationGame/src/SimpleSizedPanel.java
@@ -0,0 +1,12 @@
+
+public class SimpleSizedPanel extends SizedPanel{
+	public void paintComponent(java.awt.Graphics g){
+		g.fillRect(30,50,45,80);
+	}
+
+	public static void main(String[] args) {
+		ShowInFrame.show(new SimpleSizedPanel());
+
+	}
+
+}
