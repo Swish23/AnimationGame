9bd94d659b40b3bb7b4b971eadee48263b36c4cc
Sauer.Fabian <Sauer.Fabian@SK-448A5B5B575E>
2015-10-27 15:15:47 +0100

    Bugfixes

--- AnimationGame/src/FirstPanel.java
+++ AnimationGame/src/FirstPanel.java
@@ -1,18 +1,22 @@
 
 import javax.swing.*;
-public class FirstPanel extends JPanel{
 
-	@Override public void paintComponent(java.awt.Graphics g) {
+public class FirstPanel extends JPanel {
+
+	    int[] xPoints = {40, 60, 70, 80, 100, 80, 70, 60};
+	    int[] yPoints = {440, 430, 410, 430, 450, 470, 450, 460};
+	    int nPoints = xPoints.length;
+	
+	@Override public void paintComponent(java.awt.Graphics g){
 		g.fillRect(30,50,45,80);
-		g.fillOval(60,180,100,50);	
+		g.fillOval(60, 30, 80, 50);
+	//	g.drawPolygon(xPoints, yPoints, nPoints);
 	}
 	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	JFrame f= new JFrame();
-	f.add(new FirstPanel());
-	f.setVisible(true);
+	public static void main(String []args){
+		JFrame f=new JFrame();
+		f.add(new FirstPanel());
+		f.pack();
+		f.setVisible(true);
 	}
-
 }
